b2e1422f1a836ce5ac80cd63a9fd61f0e00fad3e
xcicoin <XCICOIN@hmamail.com>
2016-10-17 17:51:20 +0400

    New checkpoint
    New checkpoint: Block 810

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -128,6 +128,7 @@ const std::initializer_list<CheckpointData> CHECKPOINTS = {
   { 42, "4322b517faeaee1b8318b2a02a486e479da2f4c13ea82c818cca02ca1e5ea6fa" },
   { 100, "c940bba48e493433ea7f553d5a1932d61a39f2566238639333cdf6f73a2cf35c" },
   { 420, "219c934c767b35fd025e5ca31de1596a4b73fcf5bb800267d16a5f5bb4d0c213" },
+  { 810, "aedbaf54280cf3d4064452173fa77213ad2f64938b74ee170e6a7e52fc98df64" },
 };
 } // CryptoNote
 
